278230f2943d6cb126b8eba3223a58547b0d4e02
Dylan Hawley <dylanthomashawley@gmail.com>
2020-07-21 01:44:29 -0400

    added vector class

--- main.cc
+++ main.cc
@@ -7,16 +7,19 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << '\n' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
+            double r = double(i) / (image_width-1);
+            double g = double(j) / (image_height-1);
+            double b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+            int ir = 255 * r;
+            int ig = 255 * g;
+            int ib = 255 * b;
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
